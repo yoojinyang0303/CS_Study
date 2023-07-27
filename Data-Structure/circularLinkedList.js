b4ff4ed0d6c00131fd1f31e35328d651d354f007
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-07-28 00:12:23 +0900

    feat: 자료구조 - 리스트/원형 연결 리스트 구현

--- Data-Structure/circularLinkedList.js
+++ Data-Structure/circularLinkedList.js
@@ -50,7 +50,24 @@ class CircularLinkedList {
     this.size++;
   }
 
-  search(data) {}
+  search(data) {
+    let curr = this.head;
+    let count = 0;
+
+    let isStart = false;
+    while (!isStart || curr != this.head) {
+      isStart = true;
+
+      if (curr.data == data) {
+        console.log(`${count}번째에 '${data}'가 있습니다.`);
+        return;
+      }
+
+      curr = curr.next;
+      count++;
+    }
+    console.log(`'${data}'가 존재하지 않습니다.`);
+  }
 
   print() {
     let curr = this.head;
@@ -60,7 +77,6 @@ class CircularLinkedList {
       isStart = true;
       console.log(curr.data);
       curr = curr.next;
-      count++;
     }
   }
 
@@ -69,8 +85,57 @@ class CircularLinkedList {
   }
 
   isEmpty() {
-    return;
+    return !this.size;
   }
 
-  remove() {}
+  remove(data) {
+    if (this.getSize() == 1) {
+      this.head.next = this.head;
+      this.head.prev = this.head;
+
+      this.size--; //  (=) this.size = 0;
+      return;
+    }
+
+    let curr = this.head;
+    let isStart = false;
+    while (!isStart || curr !== this.head) {
+      isStart = true;
+
+      if (curr.data == data) {
+        break;
+      }
+      curr = curr.next;
+    }
+
+    curr.prev.next = curr.next;
+    curr.next.prev = curr.prev;
+
+    this.size--;
+  }
 }
+
+const circularLinkedList = new CircularLinkedList();
+
+circularLinkedList.insert(1, "화");
+circularLinkedList.print();
+circularLinkedList.insert(1, "월");
+circularLinkedList.print();
+circularLinkedList.insert(3, "목");
+circularLinkedList.print();
+circularLinkedList.insert(3, "수");
+circularLinkedList.print();
+
+circularLinkedList.search("금");
+circularLinkedList.search("수");
+
+circularLinkedList.remove("목");
+circularLinkedList.print();
+circularLinkedList.remove("화");
+circularLinkedList.print();
+circularLinkedList.remove("월");
+circularLinkedList.print();
+circularLinkedList.remove("수");
+circularLinkedList.print();
+circularLinkedList.remove("금");
+circularLinkedList.print();
